9ee88dcab8702e86cc1f93f60ecb7a8e93bbb126
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-09-04 13:00:26 +0200

    Update

--- funktionen.php
+++ funktionen.php
@@ -1,10 +1,51 @@
 <?php
 
 // Parameter der Funktion sind der Input
-function fleischwolf($input) {
-    $input .= ' gehackt';
+function fleischwolf(string $input) : string { // $input soll ein String sein, Rücgabetyp ist string
+    $input .= ' gehackt, ';
+    return $input; // Rückgabewert, Rückgabetyp string
+    //return 100; // Rückgabetyp int
+}
+
+// Eingangsparameter ist ein oder mehrere Strings
+function fleischwolf2(string ...$input) : string { // $input ist ein array, der alle Argumente enthält
+    $input = implode(', ', $input);
+    $input .= ' alles gehackt, ';
+    return $input;
+}
+
+function fleischwolf3(array $input) : string {
+    $input = implode(', ', $input);
+    $input .= ' alles gehackt, ';
     return $input;
 }
 
 $output = fleischwolf('Fleisch');
+echo $output;
+
+$output = fleischwolf('Handy');
+echo $output;
+
+$output = fleischwolf('Brot');
+echo $output;
+
+$output = fleischwolf(100); // Die 100 wird zum string konvertiert (wenn nicht strict mode aktiv ist)
+echo $output;
+
+// Rückgabewert der Inneren Funktion wird als Argument der Äußeren benutzt
+$output = fleischwolf(fleischwolf('Melone'));
+echo $output;
+
+echo '<br>';
+
+// Aufruf einer Variadischen Funktion - kann eine flexible Anzahl Parameter übernehmen (0 bis n)
+//$output = fleischwolf('Melone', 'Butter', 'Käse', 'Fleisch'); // Verarbeitet nur den ersten Parameter
+
+//$output = fleischwolf2('Melone', 'Butter', 'Käse', 'Fleisch');
+$output = fleischwolf2('Melone', 'Butter');
+$output = fleischwolf2();
+$output = fleischwolf3(array('Melone', 'Butter')); // Erwartet ein Array
+echo $output;
+
+
 ?>
