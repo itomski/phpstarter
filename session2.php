dbc04dfb0f06bddc3eb5c062d8aed57d2377a9c4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-09-05 13:19:39 +0200

    Update Bootstrap

--- session2.php
+++ session2.php
@@ -19,14 +19,26 @@ else {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="bootstrap-5.3.1/css/bootstrap.min.css">
     </head>
     <body>
         
         <form action="session2.php" method="post">
-            <label for="user">Name: </label>
-            <input type="text" name="user" id="user">
+
+            <div class="mb-3">
+                <label for="vorname" class="form-label">Vorname:</label>
+                <input type="text" name="vorname" id="vorname">
+            </div>
+
+            <div class="mb-3">
+                <label for="nachname" class="form-label">Nachname:</label>
+                <input type="text" name="nachname" id="nachname">
+            </div>
+
             <button>Senden</button>
         </form>
+
+        <script src="bootstrap-5.3.1/js/bootstrap.min.js"></script>
 
     </body>
     </html>
